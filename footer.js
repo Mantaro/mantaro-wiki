259637c3afb7900c9aac0090f8f34513b13d3c11
Haxiz <Haxiz@users.noreply.github.com>
2022-08-20 18:26:54 +0000

    deploy: 01399f90dceab423e8687e1725c7272ed8b4c222

--- footer.js
+++ footer.js
@@ -1,10 +1,87 @@
 footer = {
   "type" : "Footer",
   "content" : [ {
-    "type" : "Paragraph",
-    "content" : [ {
-      "text" : "Contributions are welcome",
-      "type" : "SimpleText"
-    } ]
+    "columns" : [ {
+      "content" : [ {
+        "align" : "left",
+        "imageSrc" : "https://cdn.discordapp.com/emojis/654322747094073365.webp",
+        "shapes" : [ ],
+        "type" : "AnnotatedImage"
+      } ]
+    }, {
+      "content" : [ {
+        "type" : "Paragraph",
+        "content" : [ {
+          "text" : "Copyright 2016-2022 ",
+          "type" : "SimpleText"
+        }, {
+          "url" : "https://github.com/Kodehawa",
+          "isFile" : false,
+          "type" : "Link",
+          "content" : [ {
+            "text" : "David Rubio Escares / Kodehawa",
+            "type" : "SimpleText"
+          } ]
+        }, {
+          "text" : ".",
+          "type" : "SimpleText"
+        } ]
+      }, {
+        "type" : "Paragraph",
+        "content" : [ {
+          "text" : "Wiki written and formatted by ",
+          "type" : "SimpleText"
+        }, {
+          "url" : "https://github.com/Haxiz",
+          "isFile" : false,
+          "type" : "Link",
+          "content" : [ {
+            "text" : "Haxiz",
+            "type" : "SimpleText"
+          } ]
+        }, {
+          "text" : " with help from ",
+          "type" : "SimpleText"
+        }, {
+          "url" : "https://github.com/MrLar",
+          "isFile" : false,
+          "type" : "Link",
+          "content" : [ {
+            "text" : "MrLar",
+            "type" : "SimpleText"
+          } ]
+        }, {
+          "text" : ".",
+          "type" : "SimpleText"
+        }, {
+          "type" : "SoftLineBreak"
+        }, {
+          "text" : "This documentation was created using ",
+          "type" : "SimpleText"
+        }, {
+          "url" : "https://github.com/testingisdocumenting/znai",
+          "isFile" : false,
+          "type" : "Link",
+          "content" : [ {
+            "text" : "Znai",
+            "type" : "SimpleText"
+          } ]
+        }, {
+          "text" : ".",
+          "type" : "SimpleText"
+        } ]
+      }, {
+        "type" : "Paragraph",
+        "content" : [ {
+          "text" : "Currently the wiki is updated to Mantaro version ",
+          "type" : "SimpleText"
+        }, {
+          "text" : "v7.0.0",
+          "type" : "TextBadge"
+        } ]
+      } ]
+    } ],
+    "config" : { },
+    "type" : "Columns"
   } ]
 }
