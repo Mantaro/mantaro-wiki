3379db3071253d20fc3fda7d1c783b0d3d4ef856
Haxiz <ld.ramirezch14@gmail.com>
2022-08-20 10:26:36 -0600

    a

---
+++ znai/public/mantaro-wiki/footer.js
@@ -0,0 +1,10 @@
+footer = {
+  "type" : "Footer",
+  "content" : [ {
+    "type" : "Paragraph",
+    "content" : [ {
+      "text" : "Contributions are welcome",
+      "type" : "SimpleText"
+    } ]
+  } ]
+}
---
+++ znai/public/mantaro-wiki/toc.js
@@ -0,0 +1,77 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "Your Product",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Your product",
+      "id" : "your-product"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter One",
+  "dirName" : "chapter-one",
+  "items" : [ {
+    "chapterTitle" : "Chapter One",
+    "pageTitle" : "Getting Started",
+    "pageMeta" : { },
+    "dirName" : "chapter-one",
+    "fileName" : "getting-started",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Main Concepts",
+      "id" : "main-concepts"
+    }, {
+      "title" : "Table of Contents",
+      "id" : "table-of-contents"
+    }, {
+      "title" : "Embedding Content",
+      "id" : "embedding-content"
+    }, {
+      "title" : "Meta",
+      "id" : "meta"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter One",
+    "pageTitle" : "Page Two",
+    "pageMeta" : { },
+    "dirName" : "chapter-one",
+    "fileName" : "page-two",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Introduction",
+      "id" : "introduction"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter Two",
+  "dirName" : "chapter-two",
+  "items" : [ {
+    "chapterTitle" : "Chapter Two",
+    "pageTitle" : "Page Three",
+    "pageMeta" : { },
+    "dirName" : "chapter-two",
+    "fileName" : "page-three",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Hello World",
+      "id" : "hello-world"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter Two",
+    "pageTitle" : "Page Four",
+    "pageMeta" : { },
+    "dirName" : "chapter-two",
+    "fileName" : "page-four",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Debugging",
+      "id" : "debugging"
+    } ]
+  } ]
+} ]
