b546d2c85d8e462ce90add2b6bf6a142132074ed
Haxiz <ld.ramirezch14@gmail.com>
2022-08-30 16:59:48 -0600

    build

--- public/mantaro-wiki/toc.js
+++ public/mantaro-wiki/toc.js
@@ -23,82 +23,7 @@ toc = [ {
     "dirName" : "basics",
     "fileName" : "FAQ",
     "viewOnRelativePath" : null,
-    "pageSectionIdTitles" : [ {
-      "title" : "1. How do I change the prefix for this bot?",
-      "id" : "1-how-do-i-change-the-prefix-for-this-bot"
-    }, {
-      "title" : "2. What is premium and what is the Patreon bot?",
-      "id" : "2-what-is-premium-and-what-is-the-patreon-bot"
-    }, {
-      "title" : "3. How does EXP in Mantaro work and what use do levels have?",
-      "id" : "3-how-does-exp-in-mantaro-work-and-what-use-do-levels-have"
-    }, {
-      "title" : "4. Some of Mantaro's commands are not showing any content (empty message), for instance on help?",
-      "id" : "4-some-of-mantaros-commands-are-not-showing-any-content-empty-message-for-instance-on-help"
-    }, {
-      "title" : "5. The bot is not responding or lagging behind what do I do?",
-      "id" : "5-the-bot-is-not-responding-or-lagging-behind-what-do-i-do"
-    }, {
-      "title" : "6. Mantaro joined a VC, but she isn't playing anything/she is lagging",
-      "id" : "6-mantaro-joined-a-vc-but-she-isnt-playing-anythingshe-is-lagging"
-    }, {
-      "title" : "7. What do the items within Mantaro do? How to earn credits? What are waifu's and how to raise my waifu value?",
-      "id" : "7-what-do-the-items-within-mantaro-do-how-to-earn-credits-what-are-waifus-and-how-to-raise-my-waifu-value"
-    }, {
-      "title" : "8. Can I add money to people in my own server/s server that I admin?",
-      "id" : "8-can-i-add-money-to-people-in-my-own-servers-server-that-i-admin"
-    }, {
-      "title" : "9. What is the Birthday announcer? Why are my birthdays not being announced? Can I set other people's Birthday?",
-      "id" : "9-what-is-the-birthday-announcer-why-are-my-birthdays-not-being-announced-can-i-set-other-peoples-birthday"
-    }, {
-      "title" : "10. Can I unclaim people who I no longer share a server with or that show as unknown user? (Waifu command)",
-      "id" : "10-can-i-unclaim-people-who-i-no-longer-share-a-server-with-or-that-show-as-unknown-user-waifu-command"
-    }, {
-      "title" : "11. When trying to waifu claim a user it says they are locked? How to get rid of claim keys?",
-      "id" : "11-when-trying-to-waifu-claim-a-user-it-says-they-are-locked-how-to-get-rid-of-claim-keys"
-    }, {
-      "title" : "12. What is dust?",
-      "id" : "12-what-is-dust"
-    }, {
-      "title" : "13. How do I change my Profile description?",
-      "id" : "13-how-do-i-change-my-profile-description"
-    }, {
-      "title" : "14. What are love letters? What are they used for? How do I get one?",
-      "id" : "14-what-are-love-letters-what-are-they-used-for-how-do-i-get-one"
-    }, {
-      "title" : "15. How do I make a custom action command like ~>hug?",
-      "id" : "15-how-do-i-make-a-custom-action-command-like-hug"
-    }, {
-      "title" : "16. Mantaro has deleted all my roles/channels/banned all my user/or similar",
-      "id" : "16-mantaro-has-deleted-all-my-roleschannelsbanned-all-my-useror-similar"
-    }, {
-      "title" : "17. How do I invite the bot? My server does not show when inviting the bot? (Or anything related to inviting the bot)",
-      "id" : "17-how-do-i-invite-the-bot-my-server-does-not-show-when-inviting-the-bot-or-anything-related-to-inviting-the-bot"
-    }, {
-      "title" : "18. What is \"Old Sparkle Pickaxe\" and can I restore/use it?",
-      "id" : "18-what-is-old-sparkle-pickaxe-and-can-i-restoreuse-it"
-    }, {
-      "title" : "19. I have been blacklisted from using the bot. What do I do? Can I be unblacklisted?",
-      "id" : "19-i-have-been-blacklisted-from-using-the-bot-what-do-i-do-can-i-be-unblacklisted"
-    }, {
-      "title" : "20. I am trying to cast an item but it is not working?",
-      "id" : "20-i-am-trying-to-cast-an-item-but-it-is-not-working"
-    }, {
-      "title" : "21. I have lost my daily can I get it back?",
-      "id" : "21-i-have-lost-my-daily-can-i-get-it-back"
-    }, {
-      "title" : "22. I just donated and I want to know how to add Mantaro Patreon or get my premium keys",
-      "id" : "22-i-just-donated-and-i-want-to-know-how-to-add-mantaro-patreon-or-get-my-premium-keys"
-    }, {
-      "title" : "23. Can I buy credits in Mantaro? Do I get credits for donating/pledging? Is there any way to obtain credits/items/badges using real money?",
-      "id" : "23-can-i-buy-credits-in-mantaro-do-i-get-credits-for-donatingpledging-is-there-any-way-to-obtain-creditsitemsbadges-using-real-money"
-    }, {
-      "title" : "24. I want to request my data / I want to delete my data",
-      "id" : "24-i-want-to-request-my-data-i-want-to-delete-my-data"
-    }, {
-      "title" : "25. I used the waifu optout option, is there a way to reverse it/optin?",
-      "id" : "25-i-used-the-waifu-optout-option-is-there-a-way-to-reverse-itoptin"
-    } ]
+    "pageSectionIdTitles" : [ ]
   }, {
     "chapterTitle" : "Basics",
     "pageTitle" : "Premium Perks",
@@ -646,8 +571,17 @@ toc = [ {
     "fileName" : "welcome-and-leave-messages",
     "viewOnRelativePath" : null,
     "pageSectionIdTitles" : [ {
-      "title" : "Getting started",
-      "id" : "getting-started"
+      "title" : "What are welcome and leave messages?",
+      "id" : "what-are-welcome-and-leave-messages"
+    }, {
+      "title" : "Important/Main commands",
+      "id" : "importantmain-commands"
+    }, {
+      "title" : "Setting your channels",
+      "id" : "setting-your-channels"
+    }, {
+      "title" : "Setting your first message",
+      "id" : "setting-your-first-message"
     }, {
       "title" : "Using Modifiers",
       "id" : "using-modifiers"
@@ -658,8 +592,8 @@ toc = [ {
       "title" : "Combining your knowledge",
       "id" : "combining-your-knowledge"
     }, {
-      "title" : "Extra",
-      "id" : "extra"
+      "title" : "Other/Extra commands",
+      "id" : "otherextra-commands"
     } ]
   }, {
     "chapterTitle" : "Guides",
@@ -738,6 +672,9 @@ toc = [ {
     }, {
       "title" : "Creating your own embed",
       "id" : "creating-your-own-embed"
+    }, {
+      "title" : "Examples",
+      "id" : "examples"
     } ]
   }, {
     "chapterTitle" : "Guides",
